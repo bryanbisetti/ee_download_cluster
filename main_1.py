581bb72c5173b34edd292680c721848132f672f7
bryanbisetti <brybisetti@gmail.com>
2026-02-22 19:11:20 +0000

    Adjust download parameters for flaring data

--- main_1.py
+++ main_1.py
@@ -7,8 +7,8 @@ df_flaring = pd.read_csv('df_flaring.csv')
 
 # Start the download process
 download_in_parallel(
-    df_flaring.iloc[:400],
-    batch_size=50,
-    max_workers=50, # The cluster can handle high concurrency
+    df_flaring.iloc[:500],
+    batch_size=5,
+    max_workers=5, # The cluster can handle high concurrency
     period="W"
 )
