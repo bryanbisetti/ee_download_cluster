52d47ac897078b41f7f21d30ffe0c6452cb9ff47
bryanbisetti <brybisetti@gmail.com>
2026-02-22 11:58:28 +0000

    Add main_2.py for data loading and parallel download

---
+++ main_2.py
@@ -0,0 +1,14 @@
+import pandas as pd
+from utils_updated import download_in_parallel
+
+
+# Load your data
+df_flaring = pd.read_csv('df_flaring.csv') 
+
+# Start the download process
+download_in_parallel(
+    df_flaring.iloc[400:800],
+    batch_size=200,
+    max_workers=200, # The cluster can handle high concurrency
+    period="W"
+)
