fc33fd828757d28f1f165764a409523e199bac8f
bryanbisetti <brybisetti@gmail.com>
2026-02-22 19:33:51 +0000

    Update main_2.py

--- main_2.py
+++ main_2.py
@@ -7,8 +7,8 @@ df_flaring = pd.read_csv('df_flaring.csv')
 
 # Start the download process
 download_in_parallel(
-    df_flaring.iloc[400:800],
-    batch_size=200,
-    max_workers=200, # The cluster can handle high concurrency
+    df_flaring.iloc[750:1050],
+    batch_size=30,
+    max_workers=30, # The cluster can handle high concurrency
     period="W"
 )
