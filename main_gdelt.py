7e08275106de47eb65c11749ffd0313c0bb9263c
bryanbisetti <brybisetti@gmail.com>
2026-03-02 21:46:39 +0000

    Create main_gdelt.py

---
+++ main_gdelt.py
@@ -0,0 +1,11 @@
+import pandas as pd
+from utils_updated import download_csv
+
+# to download links from Gdelt
+weeks = pd.date_range(start="2016-01-01", end="2018-01-01", freq="W")
+dates = weeks.strftime("%b %Y").tolist() 
+
+for i in range(0,len(weeks)-1):
+    days_param = 1
+    print((weeks[i],weeks[i+1]))
+    download_csv((weeks[i],weeks[i+1]), days_param)
